63798d61fa24c0f2f8aba44840ade4196dbc693f
Joe Le <hle@sfsu.edu>
2025-09-08 13:46:26 -0700

    Lab 1 commit

---
+++ Lab1.py
@@ -0,0 +1,9 @@
+# Lab1.py
+# Author: Joe Le
+# Description: Simple Python script that prints a message
+
+# Store the message in a variable
+message = "Go gators!"
+
+# Print the message to the terminal
+print(message)
